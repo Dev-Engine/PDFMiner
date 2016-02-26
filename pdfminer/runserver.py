5302535c6c2940f895634d9bb911e03f64134cbc
Scen <he.kang@dev-engine.com>
2016-02-26 14:09:44 +0800

    init commit

---
+++ pdfminer/runserver.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from flask import (Flask, request, session, redirect, url_for,
+                   render_template, flash)
+
+
+# create our little application :)
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run()
