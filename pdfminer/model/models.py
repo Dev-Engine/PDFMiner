6a8c5e1a41d67678ddcc72aa63ccebd66c64891b
ScenK <he.kang@dev-engine.com>
2016-04-23 11:00:15 +0800

    make model unique

--- pdfminer/model/models.py
+++ pdfminer/model/models.py
@@ -1,32 +1,1 @@
-"""."""
-# -*- coding: utf-8 -*-
-import datetime
-from flask.ext.mongoengine import MongoEngine
-
-db = MongoEngine()
-
-
-class User(db.Document):
-    """."""
-
-    name = db.StringField(max_length=50, required=True)
-    password = db.StringField()
-    email = db.StringField()
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-
-
-class Project(db.Document):
-    """."""
-
-    name = db.StringField(required=True)
-    pdfs = db.SortedListField(db.EmbeddedDocumentField('PdfEm'))
-    author = db.ReferenceField(User)
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-
-
-class PdfEm(db.EmbeddedDocument):
-    """."""
-
-    name = db.StringField(required=True)
-    author = db.ReferenceField(User)
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+"""."""# -*- coding: utf-8 -*-import datetimefrom flask.ext.mongoengine import MongoEnginedb = MongoEngine()class User(db.Document):    """."""    name = db.StringField(max_length=50, required=True, unique=True)    password = db.StringField()    email = db.StringField()    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)class Project(db.Document):    """."""    name = db.StringField(required=True, unique=True)    pdfs = db.SortedListField(db.EmbeddedDocumentField('PdfEm'))    author = db.ReferenceField(User)    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)class PdfEm(db.EmbeddedDocument):    """."""    name = db.StringField(required=True, unique=True)    author = db.ReferenceField(User)    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
